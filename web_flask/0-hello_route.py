2a9b459c50036f79dd0250471d9d69cdecc013e8
adamabdulkareem <dmabdulkareem@gmail.com>
2023-08-05 03:10:19 -0700

    Modify some files

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -12,14 +12,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
+
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
-     """
+    """
     Returns:
         Hello HBNB
     """
+    return "Hello HBNB!"
 
-    return 'Hello HBNB!'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=None)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
