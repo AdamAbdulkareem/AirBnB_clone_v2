f6fe826a5bf5e9aea292b19a48ba975901120d21
adamabdulkareem <dmabdulkareem@gmail.com>
2023-08-05 03:08:57 -0700

    Modify some files

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
-#Imoprt the Flask class from the flask module
+"""
+This script that starts a Flask web application
+    info:
+        web application must be listening on 0.0.0.0, port 5000
+        Routes:
+            /: display “Hello HBNB!”
+            uses the option strict_slashes=False in your route definition
+"""
+
 from flask import Flask
 
-#Create an instance of the Flask class with the name of
-#the current module as the argument
 app = Flask(__name__)
 
-#Define a route for the root URL ("/") of the web application
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+     """
+    Returns:
+        Hello HBNB
+    """
+
     return 'Hello HBNB!'
 
-#Check if the script is being run as the main program
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=None)
